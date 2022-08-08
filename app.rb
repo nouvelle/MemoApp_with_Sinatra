212fa98993061ddc8b0688395099c46d760e906c
nouvelle <nouvellenuts@gmail.com>
2022-08-08 23:08:53 +0900

    refactor: classの作成

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
 require "csv"
+require "./memo.rb"
 
-def memo
-  @memo = Memo.new
-end
-
+####################
+# View画面からの要求 
+####################
 get "/" do
   @title = "メモ帳 📋"
-  @data_list = CSV.read("./memo_db.csv").sort
+  @data_list = Memo.new.get
   erb :index
 end
 
@@ -19,69 +19,44 @@ end
 
 get "/show/:id" do |id|
   @title = "メモ帳 📋 - 表示"
-  data_list = CSV.read("./memo_db.csv")
-  data_list.each do |data|
-    @show_data = data if data[0] == id
-  end
+  @show_data = Memo.new.get_by_id(id)
   erb :show
 end
 
 get "/edit/:id" do |id|
   @title = "メモ帳 📋 - 修正"
-  data_list = CSV.read("./memo_db.csv")
-  data_list.each do |data|
-    @edit_data = data if data[0] == id
-  end
+  @edit_data = Memo.new.get_by_id(id)
   erb :edit
 end
 
+not_found do
+  erb :notfound
+end
+
+####################
+# API呼び出し
+####################
 post "/api/memos" do
-  @title = params[:title]
-  @contents = params[:contents]
-
-  memos = CSV.read("./memo_db.csv")
-  id = memos.map { |array| array[0] }.max
-
-  CSV.open("./memo_db.csv","a") do |csv|
-    csv << [id.to_i + 1, @title, @contents]
-  end
+  title = params[:title]
+  contents = params[:contents]
+  Memo.new.create(title, contents)
 
   redirect "/"
   erb :index
 end
 
 patch "/api/memos/:id" do |id|
-  @title = params[:title]
-  @contents = params[:contents]
-
-  memos = CSV.read("./memo_db.csv")
-  memos.delete_if { |array| array[0] == id}
-
-  CSV.open("./memo_db.csv","w") do |csv|
-    memos.each do |array|
-      csv << array
-    end
-    csv << [id, @title, @contents]
-  end
+  title = params[:title]
+  contents = params[:contents]
+  Memo.new.update(id, title, contents)
 
   redirect "/"
   erb :index
 end
 
 delete "/api/memos/:id" do |id|
-  memos = CSV.read("./memo_db.csv")
-  memos.delete_if { |array| array[0] == id}
-
-  CSV.open("./memo_db.csv","w") do |csv|
-    memos.each do |array|
-      csv << array
-    end
-  end
+  Memo.new.delete(id)
 
   redirect "/"
   erb :index
-end
-
-not_found do
-  erb :notfound
 end
---
+++ memo.rb
@@ -0,0 +1,43 @@
+class Memo
+  def initialize()
+    @data_list = CSV.read("./memo_db.csv").sort
+  end
+
+  def get
+    @data_list
+  end
+
+  def get_by_id(id)
+    result = []
+    @data_list.each do |data|
+      result = data if data[0] == id
+    end
+    result
+  end
+
+  def create(title, contents)
+    id = @data_list.map { |array| array[0] }.max
+    CSV.open("./memo_db.csv","a") do |csv|
+      csv << [id.to_i + 1, title, contents]
+    end
+  end
+
+  def update(id, title, contents)
+    @data_list.delete_if { |array| array[0] == id}
+    CSV.open("./memo_db.csv","w") do |csv|
+      @data_list.each do |array|
+        csv << array
+      end
+      csv << [id, title, contents]
+    end
+  end
+
+  def delete(id)
+    @data_list.delete_if { |array| array[0] == id}
+    CSV.open("./memo_db.csv","w") do |csv|
+      @data_list.each do |array|
+        csv << array
+      end
+    end
+  end
+end
